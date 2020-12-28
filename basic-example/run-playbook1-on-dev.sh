57067d118a2be1d667a81715f5d711a7e008176d
Gabor Antal <1g.antal@gmail.com>
2020-12-28 05:09:32 +0100

    extra vars

--- basic-example/run-playbook1-on-dev.sh
+++ basic-example/run-playbook1-on-dev.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook -i inventories/dev main.yml
+ansible-playbook -i inventories/dev playbook1.yml --extra-vars "extra1=foo extra2=bar" --extra-vars "@extra-vars.yml"
--- basic-example/run-playbook1-on-prod.sh
+++ basic-example/run-playbook1-on-prod.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook -i inventories/prod main.yml
+ansible-playbook -i inventories/prod playbook1.yml --extra-vars "extra1=foo extra2=bar" --extra-vars "@extra-vars.yml"
